f5475caf881e0db5fe440dfe958aaa44010e5aa5
claudio Nhanga <claudionhangapc@gmail.com>
2020-11-14 20:25:08 -0300

    adicionando o index

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>CnhangaStd</title>
+</head>
+<body>
+  <!-- header -->
+  <header>
+
+    <div class="header-top">
+      <div class="header-top-container">
+          <a href="">WhatsApp: 51982378569</a>
+          <a href="">Peça Orçamento</a>
+      </div>
+    </div>
+
+    <div class="menu">
+      
+      <div class="menu-container">
+        <div class="menu-container-left-side">
+          <a href="#" class="logo">
+            Cnhanga<span>Std</span>
+          </a> 
+          <input type="checkbox" id="check">
+          <label for="check" class="check-btn">
+            <i class="fa fa-bars">
+            </i>
+          </label>
+        </div>
+        
+        <nav class="class-nav">
+          <ul>
+            <li>
+              <a href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">SERVIÇO
+                <i class="fa fa-caret-down">
+                </i>
+              </a>
+              
+              <ul>
+                <li>
+                  <a href="#">ENSAIO INFANTIL</a>
+                </li>
+                <li>
+                  <a href="#">ENSAIO FAMILIA</a>
+                </li>
+                <li>
+                  <a href="#">ENSAIO PET</a>
+                </li>
+                <li>
+                  <a href="#">EDITORIAL DE MODA</a>
+                </li>
+                <li>
+                  <a href="#">CHÁ DE FRALDA</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">ORÇAMENTO</a>
+            </li>
+            <li>
+              <a href="#">SOBRE</a>
+            </li>
+            <li>
+              <a href="#">PORTIFOLIO
+                <i class="fa fa-caret-down">
+                </i>
+              </a>
+              <ul>
+                <li>
+                  <a href="#">PROJETOS RECENTE</a>
+                </li>
+                <li>
+                  <a href="#">CASAMENTOS</a>
+                </li>
+                <li>
+                  <a href="#">GESTANTE</a>
+                </li>
+                <li>
+                  <a href="#">ENSAIO DE CASADOS</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">BLOG</a>
+            </li>
+            <li>
+              <a href="#">CONTATO</a>
+            </li>   
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+  <!-- INTRODUÇAO -->
+  <section class="introducao">
+    <div class="introducao-slide">
+      <img src="assets/images/img-header-birthday.jpg" alt=""> 
+      <div class="introducao-conteudo">
+        <h1>
+          ENSAIO 15 ANOS
+        </h1>
+        <p>
+          É a chegada ao limiar da juventude. Deixar para trás a infância
+          tão próxima e preparar-se para novas responsabilidades, novas descobertas e emoções 
+        </p>
+        <a href="">
+          VER MAIS
+        </a>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
