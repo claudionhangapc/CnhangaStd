a0bdfbfd4c2a6433a2403767839bca024bb25220
claudio Nhanga <claudionhangapc@gmail.com>
2020-11-21 17:36:30 -0300

    adicionado seções novas - serviços, depoimentos e blog

--- index.php
+++ index.php
@@ -168,10 +168,133 @@
         <a href="">VER MAIS</a>
       </div>
 
-      
-
     </div>
 
   </section>
+
+  <!-- SECTION SERVIÇO -->
+  <section class="ultimos-servicos">
+
+    <h1>ULTIMOS SERVIÇOS</h1>
+    <div class="ultimos-servicos-item-1">
+      <div class="ultimos-servicos-slide">
+        <div>
+          <img src="assets/images/wedding-3.png" alt="">
+        </div>
+        <div class="ultimos-servicos-slide-margin">
+          <img src="assets/images/wedding-2.png" alt="">
+        </div>
+        <div>
+          <img src="assets/images/school.png" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="ultimos-servicos-item-2">
+      <span></span>
+      <span class="active-sapn"></span>
+      <span></span>
+    </div>
+    <div class="ultimos-servicos-item-3">
+      <p>
+        Voce pode conhecer outros serviços nosso clicando no botão abaixo 
+      </p>
+      <a href="">VER MAIS</a>
+    </div>
+  
+  </section>
+
+  <!-- SECTION SERVIÇO -->
+  <section class="depoimento">
+    <h1>DEPOIMENTOS</h1>
+    <div class="depoimento-container">
+      <div class="depoimento-container-itme-1">
+        <img src="assets/images/pedro.png" alt=""> 
+      </div>
+      <div class="depoimento-container-itme-2">
+        <p>
+          A primeira vez que eu confio em algum serviço da internet, supeou 
+          minhas expetativas, agradeço pelo trabalho e com certeza recomendareipara outras pessoas, pois não posso deixar isto so pra mim. 
+        </p>
+        <span>
+          Pedro Simão
+        </span>
+      </div>
+    </div>
+  </section>
+
+  <!-- BLOG -->
+  <section class="blog">
+    <h1>BLOG</h1>
+      <div class="blog-item-1">
+        <div class="blog-slide">
+          <div>
+            <img src="assets/images/blog.png" alt="">
+            <div class="blog-texto">
+              <h2>
+                João fotografa beleza e 
+                destruição do Pantanal e fog 
+                dos clichês.
+              </h2>
+              <span> Claudio - outubro 2018</span>
+              <p>
+                fazendo com que este seja o primeiro 
+                verdadeiro  gerador na Internet. Usa 
+                um dicionário de 200 palavras em 
+                Latim, combinado com uma dúzia de..
+              </p>
+              <a href=""> LER MAIS</a>
+            </div>
+          </div>
+          <div class="ultimos-servicos-slide-margin">
+            <img src="assets/images/blog1.png" alt="">
+            <div class="blog-texto">
+              <h2>
+                João fotografa beleza e 
+                destruição do Pantanal e fog 
+                dos clichês.
+              </h2>
+              <span> Claudio - outubro 2018</span>
+              <p>
+                fazendo com que este seja o primeiro 
+                verdadeiro  gerador na Internet. Usa 
+                um dicionário de 200 palavras em 
+                Latim, combinado com uma dúzia de..
+              </p>
+              <a href=""> LER MAIS</a>
+            </div>
+          </div>
+          <div>
+            <img src="assets/images/blog2.png" alt="">
+            <div class="blog-texto">
+              <h2>
+                João fotografa beleza e 
+                destruição do Pantanal e fog 
+                dos clichês.
+              </h2>
+              <span> Claudio - outubro 2018</span>
+              <p>
+                fazendo com que este seja o primeiro 
+                verdadeiro  gerador na Internet. Usa 
+                um dicionário de 200 palavras em 
+                Latim, combinado com uma dúzia de..
+              </p>
+              <a href=""> LER MAIS</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="blog-item-2">
+        <span></span>
+        <span class="active-sapn"></span>
+        <span></span>
+      </div>
+      <div class="blog-item-3">
+        <p>
+          Voce pode conhecer outros serviços nosso clicando no botão abaixo 
+        </p>
+        <a href="">VER MAIS</a>
+      </div>
+  </section>
+
 </body>
 </html>
