95ea639b368a5c1517e8ed139301951181cf587a
claudio Nhanga <claudionhangapc@gmail.com>
2020-11-22 19:27:15 -0300

    Finalizado a pagina home, e trabalhando ana pagina serviço

---
+++ pages-parts/footer.php
@@ -0,0 +1,75 @@
+<!-- RODAPE  -->
+<section class="rodape">
+    <div class="rodape-container">
+      <div>
+        <h4>
+          <a href="" class="rodape-titulo">Serviços</a>
+        </h4>
+        <ul>
+          <li>
+            <a href="">Ensaio Gestante e Familia</a>
+          </li>
+          <li>
+            <a href="">Ensaio infantil</a>
+          </li>
+          <li>
+            <a href="">ensaio Pet</a>
+          </li>
+          <li>
+            <a href="">Chá de Fraldas</a>
+          </li>
+          
+        </ul>
+      </div>
+      <div class="slide-margin ">
+        <h4>
+          <a href="" class="rodape-titulo">Entrar em Contato</a>
+        </h4>
+        <ul>
+          <li>
+            <a href="" >WhatsApp: 51982378569</a>
+          </li>
+          <li>
+            <a href="">Email:cnhangastd@gmail.com</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>
+          <a href="" class="rodape-titulo">Newsletter</a>
+        </h4>
+        <ul>
+          <li class="rodape-li-form">
+            <input type="text"> <input type="submit" value="ok">
+          </li>
+          <li>
+            <a href="">   
+              <i class="fa fa-facebook fa-1x" ></i>
+               Pagina do FaceBook
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i class="fa fa-instagram fa-1x" ></i>
+              Pagina do Instagram
+            </a>
+          </li>
+          <li>
+            <a href="">
+            <i class=" fa fa-map-marker fa-1x" ></i>
+              Escritorio
+            </a>
+           
+          </li>
+          <li >
+            <a href="">Independência, 1055, SL/RS</a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </section>
+
+
+</body>
+</html>
---
+++ pages-parts/servico.php
@@ -0,0 +1,114 @@
+
+<?php 
+  include "pages-parts/header.php";
+
+?>
+<section class="intro-page">
+  <h1>
+    SERVIÇOS
+  </h1>
+  <p>
+    É a chegada ao limiar da juventude. Deixar para trás a infância
+    tão próxima e preparar-se para novas responsabilidades,
+  </p>
+</section>
+<section class="page-servico">
+  <div class="page-servico-conteudo">
+    <div class="servico-conteudo-item">
+      <div class="servico-conteudo-item-div-esquerdo">
+          <img src="assets/images/ensaio-familia.png" alt=""> 
+      </div>
+      <div class="servico-conteudo-item-div-direito">
+        <h2>ENSAIO INFANTIL</h2>
+        <p>
+          Existem muitas variações das passagens 
+          do Lorem Ipsum disponíveis,mas a maior 
+          parte sofreu alterações de alguma forma,
+          pela injecção de humor, ou de palavras 
+          aleatórias que nem sequer parecem 
+          suficientemente
+        </p>
+        <ul>
+          <li class="">
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+        </ul>
+        <a href="">VER MAIS</a>  <a href="" class="class-link">ORÇAMENTO</a>
+      </div> 
+    </div>
+    <div class="servico-conteudo-item">
+      <div class="servico-conteudo-item-div-esquerdo">
+          <img src="assets/images/ensaio-familia.png" alt=""> 
+      </div>
+      <div class="servico-conteudo-item-div-direito">
+        <h2>ENSAIO INFANTIL</h2>
+        <p>
+          Existem muitas variações das passagens 
+          do Lorem Ipsum disponíveis,mas a maior 
+          parte sofreu alterações de alguma forma,
+          pela injecção de humor, ou de palavras 
+          aleatórias que nem sequer parecem 
+          suficientemente
+        </p>
+        <ul>
+          <li class="">
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+        </ul>
+        <a href="">VER MAIS</a>  <a href="" class="class-link">ORÇAMENTO</a>
+      </div> 
+    </div>
+
+    <div class="servico-conteudo-item">
+      <div class="servico-conteudo-item-div-esquerdo">
+          <img src="assets/images/ensaio-familia.png" alt=""> 
+      </div>
+      <div class="servico-conteudo-item-div-direito">
+        <h2>ENSAIO INFANTIL</h2>
+        <p>
+          Existem muitas variações das passagens 
+          do Lorem Ipsum disponíveis,mas a maior 
+          parte sofreu alterações de alguma forma,
+          pela injecção de humor, ou de palavras 
+          aleatórias que nem sequer parecem 
+          suficientemente
+        </p>
+        <ul>
+          <li class="">
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+          <li>
+            Tentar ser mais confortavel
+          </li>
+        </ul>
+        <a href="">VER MAIS</a>  <a href="" class="class-link">ORÇAMENTO</a>
+      </div> 
+    </div>
+  
+  </div>
+</section> 
+<section class="conclu-page">
+  <p>
+    É a chegada ao limiar da juventude. Deixar para trás a infância
+    tão próxima e preparar-se para novas responsabilidades,
+  </p>
+</section>
+<?php 
+  include "pages-parts/footer.php";
+
+?>
